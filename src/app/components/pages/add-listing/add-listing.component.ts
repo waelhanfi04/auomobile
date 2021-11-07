5a134d6b0e9eee0514529ec84a6a7e76258fc3e7
Mariem-Jebali <jebalimariem2018@gmail.com>
2021-11-07 09:41:16 +0000

    add car part 1

--- src/app/components/pages/add-listing/add-listing.component.ts
+++ src/app/components/pages/add-listing/add-listing.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-add-listing',
@@ -6,10 +8,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-listing.component.css']
 })
 export class AddListingComponent implements OnInit {
+  addCarForm: FormGroup;
+  step1:boolean=true;
+  step2:boolean=false;
+  step3:boolean=false;
+  step4:boolean=false;
+  errorMsg='';
+  successMsg='';
+  isSubmitting: boolean = false;
+  years:any=[]
+  constructor(private router:Router) {
+    this.addCarForm = new FormGroup({
+      title:new FormControl(''),
+      availablity:new FormControl(''),
+      phone:new FormControl(''),
+      country:new FormControl(''),
+      city:new FormControl(''),
+      brand:new FormControl(''),
+      model:new FormControl(''),
+      color:new FormControl(''),
+      carrosserie:new FormControl(''),
+      garantie:new FormControl(''),
+      month:new FormControl(''),
+      year:new FormControl(''),
+      category:new FormControl(''),
+      price:new FormControl(''),
+      description:new FormControl(''),
+    });
 
-  constructor() { }
+  }
 
   ngOnInit(): void {
+    var aujd = new Date();
+    const currentYear = aujd.getFullYear();
+
+    for (let year =1900; year <currentYear+1; year++) {
+      this.years.push(year);
+    }
   }
 
 }
