f4976babfbcb7688835be9871f194c7881bef507
Mariem-Jebali <jebalimariem2018@gmail.com>
2021-11-07 21:09:47 +0000

    add car part 2

--- src/app/components/pages/add-listing/add-listing.component.ts
+++ src/app/components/pages/add-listing/add-listing.component.ts
@@ -9,41 +9,88 @@ import { Router } from '@angular/router';
 })
 export class AddListingComponent implements OnInit {
   addCarForm: FormGroup;
-  step1:boolean=true;
-  step2:boolean=false;
-  step3:boolean=false;
-  step4:boolean=false;
-  errorMsg='';
-  successMsg='';
+  step1: boolean = true;
+  step2: boolean = false;
+  step3: boolean = false;
+  step4: boolean = false;
+  errorMsg = '';
+  successMsg = '';
   isSubmitting: boolean = false;
-  years:any=[]
-  constructor(private router:Router) {
+  years: any = []
+  powerFiscalArray:any=[]
+  constructor(private router: Router) {
     this.addCarForm = new FormGroup({
-      title:new FormControl(''),
-      availablity:new FormControl(''),
-      phone:new FormControl(''),
-      country:new FormControl(''),
-      city:new FormControl(''),
-      brand:new FormControl(''),
-      model:new FormControl(''),
-      color:new FormControl(''),
-      carrosserie:new FormControl(''),
-      garantie:new FormControl(''),
-      month:new FormControl(''),
-      year:new FormControl(''),
-      category:new FormControl(''),
-      price:new FormControl(''),
-      description:new FormControl(''),
+      title: new FormControl(''),
+      availablity: new FormControl(''),
+      phone: new FormControl(''),
+      country: new FormControl(''),
+      city: new FormControl(''),
+      brand: new FormControl(''),
+      model: new FormControl(''),
+      color: new FormControl(''),
+      carrosserie: new FormControl(''),
+      guarantee: new FormControl(''),
+      month: new FormControl(''),
+      year: new FormControl(''),
+      category: new FormControl(''),
+      price: new FormControl(''),
+      description: new FormControl(''),
+      pictures: new FormControl(''),
+      address: new FormControl(''),
+      motorization: new FormControl(''),
+      mileage: new FormControl(''),
+      energy: new FormControl(''),
+      transmission: new FormControl(''),
+      powerFiscal: new FormControl(''),
+      gearbox: new FormControl(''),
+      insideEquipment: new FormControl(''),
+      outsideEquipment: new FormControl(''),
+      securityEquipment: new FormControl(''),
+      seatingCapacity: new FormControl(''),
+      numberDoors: new FormControl(''),
     });
 
   }
-
+/*
+  title
+  availablity
+  phone
+  country
+  city
+  brand
+  model
+  price
+  color
+  carrosserie
+  guarantee
+  month
+  year
+  category
+  pictures
+  address
+  motorization
+  mileage
+  energy
+  transmission
+  powerFiscal
+  gearbox
+  description
+  seatingCapacity
+  numberDoors
+   insideEquipment
+  outsideEquipment
+  securityEquipment
+  */
   ngOnInit(): void {
     var aujd = new Date();
     const currentYear = aujd.getFullYear();
 
-    for (let year =1900; year <currentYear+1; year++) {
+    for (let year = 1900; year < currentYear + 1; year++) {
       this.years.push(year);
+    }
+
+    for (let i =4; i < 49; i++) {
+      this.powerFiscalArray.push(i);
     }
   }
 
