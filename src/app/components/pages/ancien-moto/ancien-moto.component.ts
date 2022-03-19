ffab8182757b4eab01f1d61f1eefdcfe4acbbc54
Mariem-Jebali <jebalimariem2018gmail.com>
2022-03-19 19:29:58 +0000

    updates

--- src/app/components/pages/ancien-moto/ancien-moto.component.ts
+++ src/app/components/pages/ancien-moto/ancien-moto.component.ts
@@ -15,13 +15,12 @@ export class AncienMotoComponent implements OnInit {
   }
   getMotos(){
     this.carService.getAllCars().subscribe((data:any)=>{
-      console.log('cars from home--> ',data)
       if(data!==null || data!==undefined)
       {
         // car?.type !=='occasions'
         this.motoList=data.voitures;
         this.motoList=  data.voitures.filter((car:any)=> 
-        car.status ==='accepted' && car.type ==='Occasion' && car.category===20
+        car.status ==='accepted' && car.type ==='Occasion' && car.category==='20'
         )
         this.motoList.forEach((element:any) => {
           element.brand = JSON.parse(element.brand)
